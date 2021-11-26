75ed28ae749b19c5d9b5b11079094d153dfef413
thilara <thilara.e@gmail.com>
2021-11-26 17:12:28 +0530

    Add routers and services

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 3000;
+const userRouter = require('./routes/user');
+const classRouter = require('./routes/class');
+const moduleRouter = require('./routes/module');
+
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.get('/', (req, res) => {
+  res.json({'message': 'ok'});
+})
+
+app.use('/user', userRouter);
+app.use('/class', classRouter);
+app.use('/module', moduleRouter);
+
+/* Error handler middleware */
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  console.error(err.message, err.stack);
+  res.status(statusCode).json({'message': err.message});
+
+
+  return;
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+});
