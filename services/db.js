3d5aec2dffc831f1a37fb141156fa1a085de815f
thilara <thilara.e@gmail.com>
2021-11-27 21:53:13 +0530

    DB pool config

--- services/db.js
+++ services/db.js
@@ -1,9 +1,17 @@
-const mysql = require('mysql2/promise');
-const config = require('../config');
+const mysql = require('mysql2');
+
+
+const pool = mysql.createPool({
+  host: process.env.HOST,
+  user: process.env.USER,
+  database: process.env.DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 2
+});
 
 async function query(sql, params) {
-  const connection = await mysql.createConnection(config.db);
-  const [results, ] = await connection.execute(sql, params);
+  const [results, ] = await pool.execute(sql, params);
 
   return results;
 }
