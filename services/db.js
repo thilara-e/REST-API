f83bf9aebc2152824180ea2208e25343253167be
thilara <thilara.e@gmail.com>
2021-11-28 00:11:50 +0530

    db issue resolver

--- services/db.js
+++ services/db.js
@@ -1,10 +1,11 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
 
 const pool = mysql.createPool({
   host: process.env.HOST,
   user: process.env.USER,
   database: process.env.DATABASE,
+  password: process.env.PASSWORD,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 2
